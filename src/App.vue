884b0f50a0f150d72a51cbab39f901eb409729b5
stLucky <asys4759@gmail.com>
2021-08-08 23:49:29 +0300

    Добавил фильтрацию и пагинацию

--- src/App.vue
+++ src/App.vue
@@ -139,6 +139,85 @@
       </section>
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
+        <p>
+          <button
+            class="
+              my-4
+              mx-2
+              inline-flex
+              items-center
+              py-2
+              px-4
+              border border-transparent
+              shadow-sm
+              text-sm
+              leading-4
+              font-medium
+              rounded-full
+              text-white
+              bg-gray-600
+              hover:bg-gray-700
+              transition-colors
+              duration-300
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-gray-500
+            "
+            type="button"
+            v-if="page > 1"
+            @click="page--"
+          >
+            Назад
+          </button>
+          <button
+            class="
+              my-4
+              mx-2
+              inline-flex
+              items-center
+              py-2
+              px-4
+              border border-transparent
+              shadow-sm
+              text-sm
+              leading-4
+              font-medium
+              rounded-full
+              text-white
+              bg-gray-600
+              hover:bg-gray-700
+              transition-colors
+              duration-300
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-gray-500
+            "
+            type="button"
+            v-if="hasNextPage"
+            @click="page++"
+          >
+            Вперед
+          </button>
+        </p>
+        <p>
+          Фильтр:
+          <input
+            class="
+              pr-10
+              border-gray-300
+              text-gray-900
+              focus:outline-none focus:ring-gray-500 focus:border-gray-500
+              sm:text-sm
+              rounded-md
+            "
+            type="text"
+            v-model.trim="filter"
+          />
+        </p>
+
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
             class="
@@ -149,7 +228,7 @@
               border-purple-800 border-solid
               cursor-pointer
             "
-            v-for="ticker of tickers"
+            v-for="ticker of pageTickers"
             :key="ticker.name"
             :class="{ 'border-4': selected === ticker }"
             @click="select(ticker)"
@@ -244,6 +323,8 @@
 </template>
 
 <script>
+const VISIBLE_TICKERS = 6;
+
 export default {
   name: "App",
   data: () => ({
@@ -253,8 +334,33 @@ export default {
     graph: [],
     allCoins: "",
     showAlert: false,
+    page: 1,
+    filter: "",
   }),
+
   computed: {
+    startTicker() {
+      return (this.page - 1) * VISIBLE_TICKERS;
+    },
+
+    endTicker() {
+      return this.page * VISIBLE_TICKERS;
+    },
+
+    filteredTickers() {
+      return this.tickers.filter((ticker) =>
+        ticker.name.startsWith(this.filter.toUpperCase())
+      );
+    },
+
+    pageTickers() {
+      return this.filteredTickers.slice(this.startTicker, this.endTicker);
+    },
+
+    hasNextPage() {
+      return this.filteredTickers.length > this.endTicker;
+    },
+
     normalizeGraph() {
       const maxValue = Math.max(...this.graph);
       const minValue = Math.min(...this.graph);
@@ -269,6 +375,7 @@ export default {
         (ticker) => ticker.name === this.name.toUpperCase()
       );
     },
+
     getSimilarCoins() {
       const coinsArray = Object.values(this.allCoins);
       return coinsArray.filter((coin) => {
@@ -278,11 +385,13 @@ export default {
         return coin.Symbol.startsWith(this.name.toUpperCase());
       });
     },
+
     getFullMatchCoin() {
       return this.getSimilarCoins.find(
         (coin) => coin.Symbol === this.name.toUpperCase()
       );
     },
+
     getSliceCoins() {
       const MAX_VISIBLE_COINS = 4;
       const sliceCoins = [];
@@ -303,6 +412,7 @@ export default {
 
       return sliceCoins;
     },
+
     isEmptyTooltip() {
       return this.getSliceCoins.length > 0;
     },
@@ -312,7 +422,7 @@ export default {
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const response = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key={076094e3b40b763b4bcce11a7c64c5158362c6ba8632a52204128f478e9d5202}`
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key={af3ab31d03e8c44a7c160d20b3eb721bdb4f9bd76323fc0eee13bd2e3432dba2}`
         );
 
         const data = await response.json();
@@ -325,6 +435,7 @@ export default {
         }
       }, 5000);
     },
+
     add() {
       if (!this.isHaveTicker && this.name) {
         const newTicker = {
@@ -333,7 +444,7 @@ export default {
         };
 
         this.tickers.push(newTicker);
-
+        this.filter = "";
         localStorage.setItem("coins-list", JSON.stringify(this.tickers));
         this.subscribeToUpdates(newTicker.name);
         this.name = "";
@@ -362,8 +473,36 @@ export default {
       this.graph = [];
     },
   },
+  watch: {
+    filter() {
+      this.page = 1;
+      window.history.pushState(
+        null,
+        "",
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        "",
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+  },
 
   created() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const getAllCoins = async () => {
       const response = await fetch(
         "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
